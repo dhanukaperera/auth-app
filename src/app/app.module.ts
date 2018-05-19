295d84781b038899b178737e7e3695e9aa52dec6
Dhanuka Perera <dhanukashehanperera@gmail.com>
2018-05-19 14:33:33 +0530

    add modules to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { FIREBASE } from '../environments/firebase.config';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(FIREBASE)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE = {
+    apiKey: "AIzaSyCqj_u8jQnB7b0Nqx1SlglywYycWO9FVzA",
+    authDomain: "auth-app-14c42.firebaseapp.com",
+    databaseURL: "https://auth-app-14c42.firebaseio.com",
+    projectId: "auth-app-14c42",
+    storageBucket: "auth-app-14c42.appspot.com",
+    messagingSenderId: "13233827629"
+}
